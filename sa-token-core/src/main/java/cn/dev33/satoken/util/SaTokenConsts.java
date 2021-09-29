dc6b0ba061df3481182ad4e4d5a6b12f472f9cc5
click33 <2393584716@qq.com>
2021-09-30 02:40:13 +0800

    v1.26.2 beta .

--- sa-token-core/src/main/java/cn/dev33/satoken/util/SaTokenConsts.java
+++ sa-token-core/src/main/java/cn/dev33/satoken/util/SaTokenConsts.java
@@ -13,7 +13,7 @@ public class SaTokenConsts {
 	/**
 	 * Sa-Token 当前版本号 
 	 */
-	public static final String VERSION_NO = "v1.26.1";
+	public static final String VERSION_NO = "v1.26.2";
 
 	/**
 	 * Sa-Token 开源地址 
