f4272c3ad432d32463dcd514abd0f64c24fd5fc5
click33 <2393584716@qq.com>
2021-12-28 23:35:56 +0800

    文档样式优化

--- sa-token-doc/doc/lib/docsify-plugin.js
+++ sa-token-doc/doc/lib/docsify-plugin.js
@@ -52,27 +52,35 @@ var myDocsifyPlugin = function(hook, vm) {
 		// 功能4，添加 toc 目录 
 		var dStr = "";
 		$('#main h2, #main h3, #main h4, #main h5, #main h6').each(function() {
-			// dStr += '<li>';
-			
 			$('.toc-box').append('<li class="toc-' + this.localName + '">' + this.innerHTML + '</li>');
-			
-			// $('.toc-box').append(this.outerHTML);
-			
-			
-			// dStr += '<li>' + $(this).text() + '</li>';
-			// console.log(this);
-			// console.log(dStr);
-			
-			
-			// dStr += '</li>';
 		});
-		// $('.toc-box').html(dStr);
-		
-		
 	});
 	
 	// 钩子函数：初始化并第一次加载完成数据后调用，没有参数。
 	hook.ready(function() {
+		// 将搜索框转移到右上角 
+		document.querySelector(".sear-box").innerHTML = '';
+		document.querySelector(".sear-box").append(document.querySelector(".search"));
+		document.querySelector(".search input").placeholder = '搜索…';
+		
+		// 点击input时，展开 
+		$('.sear-box input').click(function() {
+			if($('.search input').val() != '') {
+				$('.results-panel').addClass('show');
+			}
+		});
+		// 失去焦点时，收缩 
+		$('.sear-box input').blur(function() {
+			setTimeout(function() {
+				$('.results-panel').removeClass('show');
+			}, 200);
+		})
+		// 选择一项时，收缩 
+		$('.sear-box').on('click', '.matching-post', function() {
+			console.log('click……');
+			// $('.search input').val('');
+			$('.results-panel').removeClass('show');
+		});
 	});
 	
 }
