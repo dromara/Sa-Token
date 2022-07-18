54d435b457303f9d41d9eef9f7a67a31658e46d2
sikadai <sikacode@163.com>
2022-07-18 18:39:25 +0800

    添加支持redis的String作为session序列化的方式

--- sa-token-core/src/main/java/cn/dev33/satoken/session/TokenSign.java
+++ sa-token-core/src/main/java/cn/dev33/satoken/session/TokenSign.java
@@ -3,62 +3,77 @@ package cn.dev33.satoken.session;
 import java.io.Serializable;
 
 /**
- * Token 签名 Model 
- * 
+ * Token 签名 Model
+ * <p>
  * 挂在到SaSession上的token签名
- * 
- * @author kong
  *
+ * @author kong
  */
 public class TokenSign implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1406115065849845073L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1406115065849845073L;
 
-	/**
-	 * token值
-	 */
-	private String value;
+    /**
+     * token值
+     */
+    private String value;
 
-	/**
-	 * 所属设备类型
-	 */
-	private String device;
+    /**
+     * 所属设备类型
+     */
+    private String device;
 
-	/** 构建一个 */
-	public TokenSign() {
-	}
+    /**
+     * 构建一个
+     */
+    public TokenSign() {
+    }
 
-	/**
-	 * 构建一个
-	 * 
-	 * @param value  token值
-	 * @param device 所属设备类型
-	 */
-	public TokenSign(String value, String device) {
-		this.value = value;
-		this.device = device;
-	}
+    /**
+     * 构建一个
+     *
+     * @param value  token值
+     * @param device 所属设备类型
+     */
+    public TokenSign(String value, String device) {
+        this.value = value;
+        this.device = device;
+    }
 
-	/**
-	 * @return token值 
-	 */
-	public String getValue() {
-		return value;
-	}
+    /**
+     * @return token值
+     */
+    public String getValue() {
+        return value;
+    }
 
-	/**
-	 * @return 所属设备类型
-	 */
-	public String getDevice() {
-		return device;
-	}
+    /**
+     * @return 所属设备类型
+     */
+    public String getDevice() {
+        return device;
+    }
 
-	@Override
-	public String toString() {
-		return "TokenSign [value=" + value + ", device=" + device + "]";
-	}
+    /**
+     * 反序列化需要
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * 反序列化需要
+     */
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    @Override
+    public String toString() {
+        return "TokenSign [value=" + value + ", device=" + device + "]";
+    }
 
 }
