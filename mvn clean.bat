f3c331165d66fcdf31e29c8f0e879063eac3c31c
click33 <2393584716@qq.com>
2022-08-28 14:59:15 +0800

    调整clean项目

--- mvn clean.bat
+++ mvn clean.bat
@@ -5,22 +5,24 @@ call mvn clean
 :: demo模块clean
 cd sa-token-demo
 
-cd sa-token-demo-springboot & call mvn clean & cd ..
-cd sa-token-demo-webflux & call mvn clean & cd ..
-cd sa-token-demo-solon & call mvn clean & cd ..
+cd sa-token-demo-alone-redis & call mvn clean & cd ..
+cd sa-token-demo-dubbo-consumer & call mvn clean & cd ..
+cd sa-token-demo-dubbo-provider & call mvn clean & cd ..
+cd sa-token-demo-grpc & call mvn clean & cd ..
+cd sa-token-demo-jwt & call mvn clean & cd ..
 cd sa-token-demo-oauth2-client & call mvn clean & cd ..
 cd sa-token-demo-oauth2-server & call mvn clean & cd ..
 cd sa-token-demo-quick-login & call mvn clean & cd ..
-cd sa-token-demo-alone-redis & call mvn clean & cd ..
-cd sa-token-demo-thymeleaf & call mvn clean & cd ..
-cd sa-token-demo-sso-server & call mvn clean & cd ..
+cd sa-token-demo-solon & call mvn clean & cd ..
+cd sa-token-demo-springboot & call mvn clean & cd ..
+cd sa-token-demo-springboot-redis & call mvn clean & cd ..
 cd sa-token-demo-sso1-client & call mvn clean & cd ..
 cd sa-token-demo-sso2-client & call mvn clean & cd ..
 cd sa-token-demo-sso3-client & call mvn clean & cd ..
 cd sa-token-demo-sso3-client-nosdk & call mvn clean & cd ..
-cd sa-token-demo-jwt & call mvn clean & cd ..
-cd sa-token-demo-dubbo-provider & call mvn clean & cd ..
-cd sa-token-demo-dubbo-consumer & call mvn clean & cd ..
+cd sa-token-demo-sso-server & call mvn clean & cd ..
+cd sa-token-demo-thymeleaf & call mvn clean & cd ..
+cd sa-token-demo-webflux & call mvn clean & cd ..
 cd sa-token-demo-websocket & call mvn clean & cd ..
 cd sa-token-demo-websocket-spring & call mvn clean & cd ..
 
