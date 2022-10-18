25a8753887a2ab7f1d08440d5a32c6ae9927deab
click33 <2393584716@qq.com>
2022-10-18 21:27:33 +0800

    增加测试项目

---
+++ sa-token-demo/sa-token-demo-test/src/main/java/com/pj/SaTokenApplication.java
@@ -0,0 +1,21 @@
+package com.pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import cn.dev33.satoken.SaManager;
+
+/**
+ * Sa-Token 测试  
+ * @author kong
+ *
+ */
+@SpringBootApplication
+public class SaTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaTokenApplication.class, args); 
+		System.out.println("\n启动成功：Sa-Token配置如下：" + SaManager.getConfig());
+	}
+	
+}
