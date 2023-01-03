44ecbe784590ac7045e317a9c1f829ea05f97478
疯狂的狮子li <15040126243@163.com>
2023-01-03 13:20:20 +0800

    add 增加 适配 redisson 客户端 jackson 序列化 插件

---
+++ sa-token-plugin/sa-token-dao-redisson-jackson/src/main/java/cn/dev33/satoken/dao/SaSessionForJacksonCustomized.java
@@ -0,0 +1,32 @@
+package cn.dev33.satoken.dao;
+
+import cn.dev33.satoken.session.SaSession;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Jackson定制版SaSession，忽略 timeout 等属性的序列化 
+ *  
+ * @author kong
+ *
+ */
+@JsonIgnoreProperties({"timeout"})
+public class SaSessionForJacksonCustomized extends SaSession {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7600983549653130681L;
+
+	public SaSessionForJacksonCustomized() {
+		super();
+	}
+
+	/**
+	 * 构建一个Session对象
+	 * @param id Session的id
+	 */
+	public SaSessionForJacksonCustomized(String id) {
+		super(id);
+	}
+
+}
