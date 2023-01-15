34f63449f78bba9e397d52f8ecb4d4ff9a242e75
noear <noear@live.cn>
2023-01-15 09:58:20 +0800

    调整 sa-token-dao-redisx 适配（提供 base64, json 两种序列化方案）

---
+++ sa-token-plugin/sa-token-dao-redisx/src/main/java/cn/dev33/satoken/dao/SaTokenDaoOfRedisBase64.java
@@ -0,0 +1,185 @@
+package cn.dev33.satoken.dao;
+
+import cn.dev33.satoken.util.SaFoxUtil;
+import org.noear.redisx.RedisClient;
+import org.noear.redisx.plus.RedisBucket;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * SaTokenDao 的 redis 适配（可以完全精准还原所有序列化类型）
+ *
+ * @author noear
+ * @since 1.6
+ */
+public class SaTokenDaoOfRedisBase64 implements SaTokenDao {
+    private final RedisBucket redisBucket;
+
+    public SaTokenDaoOfRedisBase64(Properties props) {
+        this(new RedisClient(props));
+    }
+
+    public SaTokenDaoOfRedisBase64(RedisClient redisClient) {
+        redisBucket = redisClient.getBucket();
+    }
+
+
+    /**
+     * 获取Value，如无返空
+     */
+    @Override
+    public String get(String key) {
+        return redisBucket.get(key);
+    }
+
+    /**
+     * 写入Value，并设定存活时间 (单位: 秒)
+     */
+    @Override
+    public void set(String key, String value, long timeout) {
+        if (timeout == 0 || timeout <= SaTokenDao.NOT_VALUE_EXPIRE) {
+            return;
+        }
+        // 判断是否为永不过期
+        if (timeout == SaTokenDao.NEVER_EXPIRE) {
+            redisBucket.store(key, value, (int) SaTokenDao.NEVER_EXPIRE);
+        } else {
+            redisBucket.store(key, value, (int) timeout);
+        }
+    }
+
+    /**
+     * 修改指定key-value键值对 (过期时间不变)
+     */
+    @Override
+    public void update(String key, String value) {
+        long expire = getTimeout(key);
+        // -2 = 无此键
+        if (expire == SaTokenDao.NOT_VALUE_EXPIRE) {
+            return;
+        }
+        this.set(key, value, expire);
+    }
+
+    /**
+     * 删除Value
+     */
+    @Override
+    public void delete(String key) {
+        redisBucket.remove(key);
+    }
+
+    /**
+     * 获取Value的剩余存活时间 (单位: 秒)
+     */
+    @Override
+    public long getTimeout(String key) {
+        return redisBucket.ttl(key);
+    }
+
+    /**
+     * 修改Value的剩余存活时间 (单位: 秒)
+     */
+    @Override
+    public void updateTimeout(String key, long timeout) {
+        // 判断是否想要设置为永久
+        if (timeout == SaTokenDao.NEVER_EXPIRE) {
+            long expire = getTimeout(key);
+            if (expire == SaTokenDao.NEVER_EXPIRE) {
+                // 如果其已经被设置为永久，则不作任何处理
+            } else {
+                // 如果尚未被设置为永久，那么再次set一次
+                this.set(key, this.get(key), timeout);
+            }
+            return;
+        }
+        redisBucket.delay(key, (int) timeout);
+    }
+
+
+    /**
+     * 获取Object，如无返空
+     */
+    @Override
+    public Object getObject(String key) {
+        return redisBucket.getAndDeserialize(key);
+    }
+
+    /**
+     * 写入Object，并设定存活时间 (单位: 秒)
+     */
+    @Override
+    public void setObject(String key, Object object, long timeout) {
+        if (timeout == 0 || timeout <= SaTokenDao.NOT_VALUE_EXPIRE) {
+            return;
+        }
+        // 判断是否为永不过期
+        if (timeout == SaTokenDao.NEVER_EXPIRE) {
+            redisBucket.storeAndSerialize(key, object);
+        } else {
+            redisBucket.storeAndSerialize(key, object, (int) timeout);
+        }
+    }
+
+    /**
+     * 更新Object (过期时间不变)
+     */
+    @Override
+    public void updateObject(String key, Object object) {
+        long expire = getObjectTimeout(key);
+        // -2 = 无此键
+        if (expire == SaTokenDao.NOT_VALUE_EXPIRE) {
+            return;
+        }
+        this.setObject(key, object, expire);
+    }
+
+    /**
+     * 删除Object
+     */
+    @Override
+    public void deleteObject(String key) {
+        redisBucket.remove(key);
+    }
+
+    /**
+     * 获取Object的剩余存活时间 (单位: 秒)
+     */
+    @Override
+    public long getObjectTimeout(String key) {
+        return redisBucket.ttl(key);
+    }
+
+    /**
+     * 修改Object的剩余存活时间 (单位: 秒)
+     */
+    @Override
+    public void updateObjectTimeout(String key, long timeout) {
+        // 判断是否想要设置为永久
+        if (timeout == SaTokenDao.NEVER_EXPIRE) {
+            long expire = getObjectTimeout(key);
+            if (expire == SaTokenDao.NEVER_EXPIRE) {
+                // 如果其已经被设置为永久，则不作任何处理
+            } else {
+                // 如果尚未被设置为永久，那么再次set一次
+                this.setObject(key, this.getObject(key), timeout);
+            }
+            return;
+        }
+        redisBucket.delay(key, (int) timeout);
+    }
+
+
+    /**
+     * 搜索数据
+     */
+    @Override
+    public List<String> searchData(String prefix, String keyword, int start, int size, boolean sortType) {
+        Set<String> keys = redisBucket.keys(prefix + "*" + keyword + "*");
+        List<String> list = new ArrayList<String>(keys);
+        return SaFoxUtil.searchList(list, start, size, sortType);
+    }
+}
