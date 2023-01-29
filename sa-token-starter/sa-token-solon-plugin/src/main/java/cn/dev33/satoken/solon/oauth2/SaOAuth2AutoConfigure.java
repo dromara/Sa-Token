f1fd2f1ca57552d3cd69df826078d00bb02d18ec
noear <noear@live.cn>
2023-01-29 18:20:55 +0800

    sa-token-solon-plugin::添加sso和oauth2框架的自动化适配

---
+++ sa-token-starter/sa-token-solon-plugin/src/main/java/cn/dev33/satoken/solon/oauth2/SaOAuth2AutoConfigure.java
@@ -0,0 +1,44 @@
+package cn.dev33.satoken.solon.oauth2;
+
+import cn.dev33.satoken.oauth2.SaOAuth2Manager;
+import cn.dev33.satoken.oauth2.config.SaOAuth2Config;
+import cn.dev33.satoken.oauth2.logic.SaOAuth2Template;
+import cn.dev33.satoken.oauth2.logic.SaOAuth2Util;
+import org.noear.solon.annotation.Bean;
+import org.noear.solon.annotation.Configuration;
+import org.noear.solon.annotation.Inject;
+
+/**
+ * @author noear
+ * @since 2.0
+ */
+@Configuration
+public class SaOAuth2AutoConfigure {
+    /**
+     * 获取 OAuth2配置Bean
+     */
+    @Bean
+    public SaOAuth2Config getConfig(@Inject("${sa-token.oauth2}") SaOAuth2Config oAuth2Config) {
+        return oAuth2Config;
+    }
+
+    /**
+     * 注入OAuth2配置Bean
+     *
+     * @param saOAuth2Config 配置对象
+     */
+    @Bean
+    public void setSaOAuth2Config(@Inject(required = false) SaOAuth2Config saOAuth2Config) {
+        SaOAuth2Manager.setConfig(saOAuth2Config);
+    }
+
+    /**
+     * 注入代码模板Bean
+     *
+     * @param saOAuth2Template 代码模板Bean
+     */
+    @Bean
+    public void setSaOAuth2Interface(@Inject(required = false) SaOAuth2Template saOAuth2Template) {
+        SaOAuth2Util.saOAuth2Template = saOAuth2Template;
+    }
+}
