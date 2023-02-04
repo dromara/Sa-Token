fa2915a073b95d0b3b581a2eb4062285adc4cb54
draco1023 <draco1023@gmail.com>
2023-02-04 16:43:48 +0800

    Add final to constant fields
    So we can use then in annotations

--- sa-token-plugin/sa-token-oauth2/src/main/java/cn/dev33/satoken/oauth2/logic/SaOAuth2Consts.java
+++ sa-token-plugin/sa-token-oauth2/src/main/java/cn/dev33/satoken/oauth2/logic/SaOAuth2Consts.java
@@ -12,13 +12,13 @@ public class SaOAuth2Consts {
 	 * @author kong 
 	 */
 	public static final class Api {
-		public static String authorize = "/oauth2/authorize";
-		public static String token = "/oauth2/token";
-		public static String refresh = "/oauth2/refresh";
-		public static String revoke = "/oauth2/revoke";
-		public static String client_token = "/oauth2/client_token";
-		public static String doLogin = "/oauth2/doLogin";
-		public static String doConfirm = "/oauth2/doConfirm";
+		public static final String authorize = "/oauth2/authorize";
+		public static final String token = "/oauth2/token";
+		public static final String refresh = "/oauth2/refresh";
+		public static final String revoke = "/oauth2/revoke";
+		public static final String client_token = "/oauth2/client_token";
+		public static final String doLogin = "/oauth2/doLogin";
+		public static final String doConfirm = "/oauth2/doConfirm";
 	}
 	
 	/**
@@ -26,40 +26,40 @@ public class SaOAuth2Consts {
 	 * @author kong 
 	 */
 	public static final class Param {
-		public static String response_type = "response_type";
-		public static String client_id = "client_id";
-		public static String client_secret = "client_secret";
-		public static String redirect_uri = "redirect_uri";
-		public static String scope = "scope";
-		public static String state = "state";
-		public static String code = "code";
-		public static String token = "token";
-		public static String access_token = "access_token";
-		public static String refresh_token = "refresh_token";
-		public static String grant_type = "grant_type";
-		public static String username = "username";
-		public static String password = "password";
-		public static String name = "name";
-		public static String pwd = "pwd";
+		public static final String response_type = "response_type";
+		public static final String client_id = "client_id";
+		public static final String client_secret = "client_secret";
+		public static final String redirect_uri = "redirect_uri";
+		public static final String scope = "scope";
+		public static final String state = "state";
+		public static final String code = "code";
+		public static final String token = "token";
+		public static final String access_token = "access_token";
+		public static final String refresh_token = "refresh_token";
+		public static final String grant_type = "grant_type";
+		public static final String username = "username";
+		public static final String password = "password";
+		public static final String name = "name";
+		public static final String pwd = "pwd";
 	}
 
 	/**
 	 * 所有返回类型 
 	 */
 	public static final class ResponseType {
-		public static String code = "code";
-		public static String token = "token";
+		public static final String code = "code";
+		public static final String token = "token";
 	}
 
 	/**
 	 * 所有授权类型 
 	 */
 	public static final class GrantType {
-		public static String authorization_code = "authorization_code";
-		public static String refresh_token = "refresh_token";
-		public static String password = "password";
-		public static String client_credentials = "client_credentials";
-		public static String implicit = "implicit";
+		public static final String authorization_code = "authorization_code";
+		public static final String refresh_token = "refresh_token";
+		public static final String password = "password";
+		public static final String client_credentials = "client_credentials";
+		public static final String implicit = "implicit";
 	}
 	
 	/** 表示OK的返回结果 */
