0451bca5b2baa323b7d50bd31a5399d52a71a2ba
Enaium <wyy-666@hotmail.com>
2023-04-04 15:09:54 +0800

    拦截器

---
+++ sa-token-plugin/sa-token-context-vertx/src/main/java/cn/dev33/satoken/context/vertx/handler/SaContextHandler.java
@@ -0,0 +1,17 @@
+package cn.dev33.satoken.context.vertx.handler;
+
+import cn.dev33.satoken.SaManager;
+import cn.dev33.satoken.context.vertx.context.SaContextForVertx;
+import io.vertx.core.Handler;
+import io.vertx.ext.web.RoutingContext;
+
+/**
+ * @author Enaium
+ */
+public class SaContextHandler implements Handler<RoutingContext> {
+  @Override
+  public void handle(RoutingContext context) {
+    SaManager.setSaTokenContext(new SaContextForVertx(context));
+    context.next();
+  }
+}
