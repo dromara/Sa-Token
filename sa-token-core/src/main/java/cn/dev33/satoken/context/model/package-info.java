017546bedfa0c01a579e0bf5da8000014486e506
click33 <2393584716@qq.com>
2023-05-15 19:09:23 +0800

    add license header

--- sa-token-core/src/main/java/cn/dev33/satoken/context/model/package-info.java
+++ sa-token-core/src/main/java/cn/dev33/satoken/context/model/package-info.java
@@ -2,4 +2,19 @@
  * 因为不能确定最终运行的 web 容器属于标准 Servlet 模型还是非 Servlet 模型，特封装此包下的包装类进行对接。
  * 调用路径为：Sa-Token 功能函数 -> SaRequest 封装接口 -> SaRequest 具体实现类。
  */
+/*
+ * Copyright 2020-2099 sa-token.cc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cn.dev33.satoken.context.model;
