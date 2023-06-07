b43fa1a1c98626fc45bdf63da65de0e3e5546a82
click33 <2393584716@qq.com>
2023-06-07 14:18:24 +0800

    ...

--- sa-token-demo/sa-token-demo-test/src/main/java/com/pj/SaTokenApplication.java
+++ sa-token-demo/sa-token-demo-test/src/main/java/com/pj/SaTokenApplication.java
@@ -1,7 +1,6 @@
 package com.pj;
 
 import cn.dev33.satoken.SaManager;
-import cn.dev33.satoken.strategy.SaStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,13 +16,6 @@ public class SaTokenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SaTokenApplication.class, args);
 		System.out.println("\n启动成功：Sa-Token配置如下：" + SaManager.getConfig());
-
-		// SaStrategy全局单例，所有方法都用以下形式重写
-		SaStrategy.instance.setCreateToken((loginId, loginType) -> {
-			// 自定义Token生成的算法
-			return "xxxx";
-		});
-
 	}
 
 }
