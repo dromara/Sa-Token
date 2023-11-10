49e51a2114fea29379057722ac374253219cc36f
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2023-11-10 14:21:40 +0800

    pref: 简化SpringBootVersionCompatibilityChecker代码

--- sa-token-starter/sa-token-spring-boot-starter/src/main/java/cn/dev33/satoken/spring/SpringBootVersionCompatibilityChecker.java
+++ sa-token-starter/sa-token-spring-boot-starter/src/main/java/cn/dev33/satoken/spring/SpringBootVersionCompatibilityChecker.java
@@ -7,14 +7,9 @@ public class SpringBootVersionCompatibilityChecker {
 
     public SpringBootVersionCompatibilityChecker() {
         String version = SpringBootVersion.getVersion();
-        if (SaFoxUtil.isEmpty(version)) {
+        if (SaFoxUtil.isEmpty(version) || version.startsWith("1.") || version.startsWith("2.")) {
             return;
         }
-        if (version.startsWith("1.") || version.startsWith("2.")) {
-            return;
-        }
-        BootstrapMethodError error = new BootstrapMethodError("当前Spring-Boot版本为" + version + "，请尝试改用：sa-token-spring-boot3-starter");
-        error.fillInStackTrace();
-        throw error;
+        throw new BootstrapMethodError("当前Spring-Boot版本为" + version + "，请尝试改用：sa-token-spring-boot3-starter");
     }
 }
