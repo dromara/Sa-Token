3f59792487e51eeabd959fa479d22d49309e354b
click33 <2393584716@qq.com>
2024-04-01 06:19:37 +0800

    修复错误代码

--- sa-token-plugin/sa-token-redisx/src/main/java/cn/dev33/satoken/dao/SaTokenDaoOfRedisBase64.java
+++ sa-token-plugin/sa-token-redisx/src/main/java/cn/dev33/satoken/dao/SaTokenDaoOfRedisBase64.java
@@ -101,7 +101,7 @@ public class SaTokenDaoOfRedisBase64 implements SaTokenDao {
      */
     @Override
     public Object getObject(String key) {
-        return redisBucket.getAndDeserialize(key, Object.class);
+        return redisBucket.getAndDeserialize(key);
     }
 
     /**
