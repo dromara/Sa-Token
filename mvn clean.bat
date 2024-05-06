cc9fe4c08bd0ca56f0e593ff208ac52581365dd0
click33 <2393584716@qq.com>
2024-05-06 10:57:57 +0800

    补全遗漏项目

--- mvn clean.bat
+++ mvn clean.bat
@@ -2,33 +2,48 @@
 :: 整体clean
 call mvn clean
 
+
 :: demo模块clean
 cd sa-token-demo
 
 cd sa-token-demo-alone-redis & call mvn clean & cd ..
 cd sa-token-demo-alone-redis-cluster & call mvn clean & cd ..
+cd sa-token-demo-beetl & call mvn clean & cd ..
+cd sa-token-demo-bom-import & call mvn clean & cd ..
 cd sa-token-demo-case & call mvn clean & cd ..
 cd sa-token-demo-grpc & call mvn clean & cd ..
+cd sa-token-demo-hutool-timed-cache & call mvn clean & cd ..
 cd sa-token-demo-jwt & call mvn clean & cd ..
 cd sa-token-demo-quick-login & call mvn clean & cd ..
 cd sa-token-demo-solon & call mvn clean & cd ..
+cd sa-token-demo-solon-redisson & call mvn clean & cd ..
 cd sa-token-demo-springboot & call mvn clean & cd ..
 cd sa-token-demo-springboot3-redis & call mvn clean & cd ..
 cd sa-token-demo-springboot-redis & call mvn clean & cd ..
 cd sa-token-demo-springboot-redisson & call mvn clean & cd ..
+cd sa-token-demo-ssm & call mvn clean & cd ..
 cd sa-token-demo-test & call mvn clean & cd ..
 cd sa-token-demo-thymeleaf & call mvn clean & cd ..
-cd sa-token-demo-beetl & call mvn clean & cd ..
 cd sa-token-demo-webflux & call mvn clean & cd ..
 cd sa-token-demo-webflux-springboot3 & call mvn clean & cd ..
 cd sa-token-demo-websocket & call mvn clean & cd ..
 cd sa-token-demo-websocket-spring & call mvn clean & cd ..
-cd sa-token-demo-bom-import & call mvn clean & cd ..
 
-cd sa-token-demo-hutool-timed-cache & call mvn clean & cd ..
-cd sa-token-demo-ssm & call mvn clean & cd ..
+cd sa-token-demo-dubbo
+cd sa-token-demo-dubbo-consumer & call mvn clean & cd ..
+cd sa-token-demo-dubbo-provider & call mvn clean & cd ..
+cd sa-token-demo-dubbo3-consumer & call mvn clean & cd ..
+cd sa-token-demo-dubbo3-provider & call mvn clean & cd ..
+cd ..
 
+cd sa-token-demo-oauth2
+cd sa-token-demo-oauth2-client & call mvn clean & cd ..
+cd sa-token-demo-oauth2-server & call mvn clean & cd ..
+cd ..
 
+cd sa-token-demo-remember-me
+cd sa-token-demo-remember-me-server & call mvn clean & cd ..
+cd ..
 
 cd sa-token-demo-sso
 cd sa-token-demo-sso-server & call mvn clean & cd ..
@@ -45,31 +60,6 @@ cd sa-token-demo-sso2-client-solon & call mvn clean & cd ..
 cd sa-token-demo-sso3-client-solon & call mvn clean & cd ..
 cd sa-token-demo-sso-server-solon & call mvn clean & cd ..
 cd ..
-
-cd sa-token-demo-oauth2
-cd sa-token-demo-oauth2-client & call mvn clean & cd ..
-cd sa-token-demo-oauth2-server & call mvn clean & cd ..
-cd ..
-
-:: cd sa-token-demo-cross
-:: cd sa-token-demo-cross-header-server & call mvn clean & cd ..
-:: cd sa-token-demo-cross-cookie-server & call mvn clean & cd ..
-:: cd ..
-
-cd sa-token-demo-dubbo
-cd sa-token-demo-dubbo-consumer & call mvn clean & cd ..
-cd sa-token-demo-dubbo-provider & call mvn clean & cd ..
-cd sa-token-demo-dubbo3-consumer & call mvn clean & cd ..
-cd sa-token-demo-dubbo3-provider & call mvn clean & cd ..
-cd ..
-
-cd sa-token-demo-remember-me
-cd sa-token-demo-remember-me-server & call mvn clean & cd ..
-cd ..
-
-
-
-
 
 
 
