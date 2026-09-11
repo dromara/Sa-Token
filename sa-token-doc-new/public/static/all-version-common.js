fadb830f46bf7933918d0d27274c4ef07820ea6e
click33 <2393584716@qq.com>
2026-09-11 22:00:22 +0800

    chore(doc): 停用百度搜索引擎自动提交脚本

--- sa-token-doc-new/public/static/all-version-common.js
+++ sa-token-doc-new/public/static/all-version-common.js
@@ -225,15 +225,15 @@
 	})();
 
 	/* 搜索引擎自动提交 */
-	(function() {
-		var bp = document.createElement('script');
-		var curProtocol = window.location.protocol.split(':')[0];
-		if (curProtocol === 'https') {
-			bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-		} else {
-			bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-		}
-		var s = document.getElementsByTagName("script")[0];
-		s.parentNode.insertBefore(bp, s);
-	})();
+	// (function() {
+	// 	var bp = document.createElement('script');
+	// 	var curProtocol = window.location.protocol.split(':')[0];
+	// 	if (curProtocol === 'https') {
+	// 		bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+	// 	} else {
+	// 		bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+	// 	}
+	// 	var s = document.getElementsByTagName("script")[0];
+	// 	s.parentNode.insertBefore(bp, s);
+	// })();
 })();
